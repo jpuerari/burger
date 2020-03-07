53a4caf906810525fd451bf745d09b6cbc80cfd4
Joseph D. Puerari <joep@Joes-MacBook-Pro.local>
2020-03-07 12:20:11 -0500

    testing

---
+++ server.js
@@ -0,0 +1,39 @@
+//Part 1:
+const path = require("path");
+const fs = require("fs");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
